cc7188e3e605a40443910e472df47f95fe917c59
CloudyGIF <anna22post@gmail.com>
2025-07-13 21:50:06 +0100

    Car Insurance

---
+++ CarInsurance/CarInsurance/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace CarInsurance
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt32(Console.ReadLine()); //gets the age num and casts to int
+            Console.WriteLine("Have you ever had a DUI?");
+            bool DUI = Convert.ToBoolean(Console.ReadLine()); //gets the true or false value and casts to bool
+            Console.WriteLine("How many speeding tickets do you have?");
+            int tickets = Convert.ToInt32(Console.ReadLine()); //gets the tickets num and casts to int
+
+            bool qualified = (age > 15) && (DUI == false) && (tickets <= 3);
+            Console.WriteLine("Qualified? \n" + qualified);
+        }
+    }
+}
