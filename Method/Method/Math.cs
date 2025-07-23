aa16ef8f2cf341c7084cf7ee48189e739836e17c
CloudyGIF <anna22post@gmail.com>
2025-07-23 23:40:22 +0100

    methods

---
+++ Method/Method/Math.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method
+{
+    internal class Math
+    {
+        
+           
+
+       
+
+            public int MathOperation(int num)
+            {
+                return num + 10; //int method
+            }
+
+            public decimal MathOperation(decimal num, decimal num2)
+        {
+            return  num + num2 + 10.1234m; //decimal parameters method overload
+        }
+
+        public int MathOperation(string text)
+        {
+            int text_num = Convert.ToInt32(text); //string parameter method overload
+
+            return 11 + text_num;
+        }
+
+
+    }
+}
---
+++ Method/Method/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Math class_instance = new Math(); //Math class is initialized 
+
+            int result = class_instance.MathOperation(19); //returns integer operation
+
+            Console.WriteLine(result);
+
+            decimal result2 = class_instance.MathOperation(19.123m, 123.213m); //returns decimal operation
+
+            Console.WriteLine(result2);
+
+            int result3 = class_instance.MathOperation("20"); //returns string operation
+
+            Console.WriteLine(result3);
+
+        }
+    }
+}
