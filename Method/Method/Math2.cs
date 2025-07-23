9891c017fa63e013e913974f2e766cecd707b644
CloudyGIF <anna22post@gmail.com>
2025-07-24 00:09:15 +0100

    assignment2

---
+++ Method/Method/Math2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method
+{
+    public class Math2
+    {
+        public int MathOperation(int num, int num2 = 0) //makes num2 by default 0 , if nothing is input
+        {
+            if(num2 == 0)
+            {
+                return (num * 200)/2;
+            }else
+            {
+                return (num * num2 * 200) / 2;
+            }
+        }
+    }
+}
--- Method/Method/Program.cs
+++ Method/Method/Program.cs
@@ -24,6 +24,35 @@ namespace Method
 
             Console.WriteLine(result3);
 
+            Math2 class_instance2 = new Math2();
+
+            Console.WriteLine("Please choose your first number");
+
+            int param1 = Convert.ToInt32(Console.ReadLine()); //gets the first input number
+
+            Console.WriteLine("Please choose your second number, however it is optional");
+
+            string param2 = Console.ReadLine();
+
+            int result4;
+
+            if (param2 == "") //turns out enter is not null but "", so param2 is a string format to accomadate that and not result in Format error.
+            {
+                result4 = class_instance2.MathOperation(param1); //if the second input is "" then only 1 parameter is passed
+            } else
+            {
+                result4 = class_instance2.MathOperation(param1, Convert.ToInt32(param2)); //if the second input is not blank then 2 parameters are parsed
+            }
+
+            Console.WriteLine(result4);
+
+            
+            
+
+            
+
+
+
         }
     }
 }
