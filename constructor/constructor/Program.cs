33c0780b2bca39ad8a89cd99b709e49101fce2f0
CloudyGIF <anna22post@gmail.com>
2025-08-13 01:17:28 +0100

    variable

---
+++ constructor/constructor/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace constructor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            const int num = 10;
+
+            var animal = "dog";
+
+            string result = num + " " + animal + "s";
+
+            Console.WriteLine(result);
+        }
+
+
+    }
+}
